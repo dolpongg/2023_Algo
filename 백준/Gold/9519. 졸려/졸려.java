f3b6568e64782fe64356ada3d61d5a13ba5b5efd
dolpongg <96533697+dolpongg@users.noreply.github.com>
2023-07-19 11:23:50 +0900

    [Gold V] Title: 졸려, Time: 196 ms, Memory: 23684 KB -BaekjoonHub

---
+++ 백준/Gold/9519. 졸려/졸려.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static int N;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder(br.readLine());
+		String input = sb.toString();
+		int len = sb.length();
+		
+		LinkedHashSet<String> set = new LinkedHashSet<>();
+		set.add(input);
+		for(int n = 0; n < N; n++) {
+			StringBuilder front = new StringBuilder();
+			StringBuilder back = new StringBuilder();
+			
+			for(int i = 0; i < len; i++) {
+				if(i%2 == 0) front.append(sb.charAt(i));
+				else back.append(sb.charAt(i));
+			}
+			
+			sb = new StringBuilder();
+			sb.append(front).append(back.reverse());
+			
+			if(!set.add(sb.toString())) break;
+		}
+		N = N%set.size();
+		
+		System.out.println(set.toArray()[N]);
+		
+	}
+	
+}
+//4
+//acefdb
