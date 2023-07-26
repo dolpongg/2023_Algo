bd15c550002a936cfb992ca1cd4b7905d65b377e
dolpongg <96533697+dolpongg@users.noreply.github.com>
2023-07-26 18:07:55 +0900

    [Gold IV] Title: 좋다, Time: 136 ms, Memory: 12708 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,52 +1,50 @@
 import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 	
-	static int N;
-	static int[] numArr;
-	static int answer = 0;
-	public static void main(String[] args) throws NumberFormatException, IOException {
+    static int N;
+    static int[] arr;
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int answer = 0;
+        
+        arr = new int[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arr);
+        
+       outer:for(int target = N-1; target >= 0; target--){
+            int start = 0;
+            int end = N-1;
+            
+            while(start < end){
+                if(start == target){
+                    start++;
+                    continue;
+                }
+                if(end == target){
+                    end--;
+                    continue;
+                }
+                int sum = arr[start] + arr[end];
+                if(sum == arr[target]){
+                    answer++;
+                    continue outer;
+                }
+                if(sum < arr[target]) start++;
+                else end--;
+            }
+        }
+        
+        System.out.println(answer);
 		
-		N = Integer.parseInt(br.readLine());
-		numArr = new int[N];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < N; i++) {
-			numArr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(numArr);
-		
-		for(int i = 0; i < N; i++) {
-			int left = 0;
-			int right = N-1;
-			
-			while(left < right) {
-				if(left == i) {
-					left++;
-					continue;
-				}
-				if(right == i) {
-					right--;
-					continue;
-				}
-				
-				if(numArr[i] > numArr[left] + numArr[right]) {
-					left++;
-				}else if(numArr[i] == numArr[left] + numArr[right]) {
-					answer++;
-					break;
-				}else {
-					right--;
-				}
-			}
-		}
-		
-		System.out.println(answer);
 	}
-
-	
 	
 }
