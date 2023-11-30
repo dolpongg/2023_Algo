cf83b13c6225759458caeca18037269c3289a1f7
dolpongg <96533697+dolpongg@users.noreply.github.com>
2023-11-30 18:22:55 +0900

    [Silver III] Title: 계단 오르기, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] stairs = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            stairs[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[n + 1];
+        dp[1] = stairs[1];
+        
+
+        for (int i = 2; i <= n; i++) {
+            if(i==2){
+                dp[2] = stairs[1] + stairs[2];
+            }else if(i==3){
+                dp[3] = Math.max(stairs[1], stairs[2]) + stairs[3];
+            }else{
+                //계단은 한 번에 한 계단, 두 계단씩만 오를 수 있다
+                //연속된 3개의 계단을 밟으면 안된다
+                dp[i] = Math.max(dp[i-3] + stairs[i-1], dp[i-2]) + stairs[i];
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
